fc8076250cf0923715461886eaaff8ea8b8a0a66
Vincent Landgraf <setcool@gmx.de>
2010-09-26 18:38:18 +0200

    implemented first part of command create

--- lib/bup/commands.rb
+++ lib/bup/commands.rb
@@ -8,26 +8,36 @@ module Bup::Commands
       FileUtils.mkdir_p dir unless File.exist? dir
       FileUtils.cp(app.class.config_template, app.filename)
     else
-      app.error "the configuration file #{app.filename} already exists!"
+      app.error "the configuration file '#{app.filename}' already exists!"
     end
   end
   
   # edit the configuration file with the default editor
   def self.edit(app)
     if File.exists? app.filename
-      if ENV["EDITOR"]
-        system(ENV["EDITOR"] + " #{app.filename}")
+      if editor = ENV["EDITOR"]
+        system("#{editor} #{app.filename}")
       else
         app.error "the environment variable EDITOR is unset. Can't open editor!"
       end
     else
-      app.error "the configuration file #{app.filename} doesn't exists!"
+      app.error "the configuration file '#{app.filename}' doesn't exist!"
     end
   end
 
   # create a new backup with name
   def self.create(app, name)
-    
+    config = app.config
+    if backup = config.backups[name]
+      if location = config.locations[backup.options[:to]]
+        Bup::Dar.create(:backup => backup, :location => location)
+      else#
+        app.error "the location '#{backup.options[:to]}' doesn't exist " \
+                  "for backup '#{name}'"
+      end
+    else
+      app.error "the backup '#{name}' doesn't exist!"
+    end
   end
   
   STRATEGIE_NAMES = {
